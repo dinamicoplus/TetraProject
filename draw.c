46cc81cf9f1475fb09e4acc9ac444568446d3dab
Marcos <marbri91@gmail.com>
2017-07-25 23:20:15 +0200

    Refactor code to follow google style convention
    Will make the code more consistent and organized. A copy of the
    google c++ style guide followed can be found here:
    https://google.github.io/styleguide/cppguide.html

--- draw.c
+++ draw.c
@@ -8,207 +8,197 @@
 
 #include "main.h"
 
-
-int init_drawing()
-{
-    initscr();
-    keypad(stdscr, TRUE);
-    (void)noecho();
-    start_color();
-    
-    init_pair(2, COLOR_WHITE, COLOR_RED);
-    init_pair(1, COLOR_WHITE, COLOR_BLUE);
-    init_pair(3, COLOR_BLACK,COLOR_WHITE);
-    init_pair(4, COLOR_WHITE,COLOR_BLACK);
-    init_pair(5, COLOR_RED, COLOR_CYAN);
-    init_pair(6, COLOR_WHITE, COLOR_CYAN);
-    return 0;
-}
-int draw_table(int x,int y)
-{
-    attron(COLOR_PAIR(3));
-    move(y,x);
-    int i,j;
-    for (i=0;i<41;i++)
-    {
-        for(j=0;j<27;j++)
-        {
-            addch('+');
-            move(y+j,x+i);
-        }
-    }
-    addch('+');
-    for(i=0;i<4;i++)
-    {
-        for(j=0;j<4;j++)
-        {
-            draw_cursor_a(x+1+10*i,y+1+6*j);
-        }
-    }
-    attroff(COLOR_PAIR(3));
-    refresh();
-    return 0;
-}
-int draw_cursor_a(int x, int y)
-{
-    attron(COLOR_PAIR(3));
-    int i=0;
-    move(y,x);
-    addstr("+-------+");
-    for(i=0;i<5;i++)
-    {
-        move(1+y+i,x);
-        addstr("|       |");
-    }
-    move(6+y,x);
-    addstr("+-------+");
-    move(0,0);
-    attroff(COLOR_PAIR(3));
-    refresh();
-    return 0;
-}
-int draw_cursor_b(int x, int y,int color)
-{
-    attron(COLOR_PAIR(color));
-    int i=0;
-    move(y,x);
-    addstr("+-------+");
-    for(i=0;i<5;i++)
-    {
-        move(1+i+y,x);
-        addstr("|       |");
-    }
-    move(6+y,x);
-    addstr("+-------+");
-    move(0,0);
-    attroff(COLOR_PAIR(color));
-    refresh();
-    return 0;
-}
-int draw_card(struct card_t *card,int x, int y)
-{
-    attron(COLOR_PAIR(card->eq+1));
-    int i,j;
-    for(i=0;i<7;i++)
-    {
-        for(j=0;j<5;j++)
-        {
-            move(y+j,x+i);
-            addch(' ');
-        }
-    }
-    
-    move(3+y,x);
-    printw(" %X  %X%X",card->stats[0],card->stats[1],card->stats[2]);
-    move(3+y,2+x);
-    switch (card->type)
-    {
-        case P: addch('P'); break;
-        case M: addch('M'); break;
-        case X: addch('X'); break;
-        case A: addch('A'); break;
-    }
-    for (i=0; i<8; i++) {
-		if(card->arrows&(1<<i))
-		{
-			draw_arrows(i, x, y, card->eq+1);
-		}
-    }
-    move(0,0);
-    attroff(COLOR_PAIR(card->eq+1));
-    refresh();
-    return 0;
+int init_drawing() {
+  initscr();
+  keypad(stdscr, TRUE);
+  (void)noecho();
+  start_color();
+  init_pair(2, COLOR_WHITE, COLOR_RED);
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  init_pair(3, COLOR_BLACK, COLOR_WHITE);
+  init_pair(4, COLOR_WHITE, COLOR_BLACK);
+  init_pair(5, COLOR_RED, COLOR_CYAN);
+  init_pair(6, COLOR_WHITE, COLOR_CYAN);
+  return 0;
 }
 
-int draw_arrows(int i,int x, int y, int color)
-{
-    char arrows[]="\\|/--/|\\";
-    int j=(((6*(7-i)/5)/3)*2);
-    int k=(((5*(7-i)/4)%3)*3);
-    attron(COLOR_PAIR(color));
-    move(j+y,k+x);
-    addch(arrows[i]);
-    move(0,0);
-    attroff(COLOR_PAIR(color));
-    refresh();
-    return 0;
+int draw_table(int x, int y) {
+  attron(COLOR_PAIR(3));
+  move(y, x);
+  int i, j;
+  for (i = 0; i < 41; i++) {
+    for (j = 0; j < 27; j++) {
+      addch('+');
+      move(y + j, x + i);
+    }
+  }
+  addch('+');
+  for (i = 0; i < 4; i++) {
+    for (j = 0; j < 4; j++) {
+      draw_cursor_a(x + 1 + 10 * i, y + 1 + 6 * j);
+    }
+  }
+  attroff(COLOR_PAIR(3));
+  refresh();
+  return 0;
 }
-int redraw(struct state_t *state)
-{
-	
-	if (state->phase==SELECTION_P)
-	{
-		clear();
-		draw_table(winx,winy);
-		
-		draw_cursor_b(winx+1+10*state->x,winy+1+6*state->y,4);
-		draw_cursor_b(winx+44-(44+12)*(state->selection/5)-1,winy+6*(state->selection%5)-1,6);
-		int i,j;
-		for (i=0;i<4;i++)
-		{
-			for(j=0;j<4;j++)
-			{
-				if(state->table[i][j]>1)
-				{
-					draw_card(&state->cards[0][state->table[i][j]-2],winx+2+10*i,winy+2+6*j);
-				}
-				if(state->table[i][j]>6)
-				{
-					draw_card(&state->cards[1][state->table[i][j]-7],winx+2+10*i,winy+2+6*j);
-				}
-			}
-		}
-		for (i=0;i<5;i++)
-		{
-			if(state->cards[0][i].played==0) {draw_card(&state->cards[0][i],winx+44,winy+6*i);}
-			if(state->cards[1][i].played==0) {draw_card(&state->cards[1][i],winx-12,winy+6*i);}
-		}
-	}
-	if (state->phase==BATTLE_P)
-	{
-		draw_card(state->battle.att,winx+2+10*state->battle.att->x,winy+2+6*state->battle.att->y);
-		draw_arrows(state->evaluation.a_arr, winx+2+10*state->battle.att->x, winy+2+6*state->battle.att->y, 5);
-		move(20,10);
-		printw("Batalla ");
-		switch (state->battle.att->type) {
-			case P:
-				printw("física: ");
-				break;
-			case M:
-				printw("mágica: ");
-				break;
-			case X:
-				printw("X: ");
-				break;
-			case A:
-				printw("Alfa: ");
-				break;
-			default:
-				break;
-		}
-		printw("%X ",state->battle.param_att);
-		if (state->battle.resul==WIN) {
-			printw("es mayor que ");
-		} else if (state->battle.resul==LOSE){
-			printw("es menor que ");
-		}
-		printw("%X",state->battle.param_def);
-		getch();
-	}
-	if (state->phase==COMBI_P)
-	{
-		if(state->battle.combi>0)
-		{
-			move(21,10);
-			printw("COMBI %d !!!",state->battle.combi);
-			getch();
-		}
-	}
-	refresh();
-	return 0;
+
+int draw_cursor_a(int x, int y) {
+  attron(COLOR_PAIR(3));
+  int i = 0;
+  move(y, x);
+  addstr("+-------+");
+  for (i = 0; i < 5; i++) {
+    move(1 + y + i, x);
+    addstr("|       |");
+  }
+  move(6 + y, x);
+  addstr("+-------+");
+  move(0, 0);
+  attroff(COLOR_PAIR(3));
+  refresh();
+  return 0;
 }
-int end_drawing()
-{
-    endwin();
-    return 0;
+
+int draw_cursor_b(int x, int y, int color) {
+  attron(COLOR_PAIR(color));
+  int i = 0;
+  move(y, x);
+  addstr("+-------+");
+  for (i = 0; i < 5; i++) {
+    move(1 + i + y, x);
+    addstr("|       |");
+  }
+  move(6 + y, x);
+  addstr("+-------+");
+  move(0, 0);
+  attroff(COLOR_PAIR(color));
+  refresh();
+  return 0;
+}
+
+int draw_card(struct card_t *card, int x, int y) {
+  attron(COLOR_PAIR(card->eq + 1));
+  int i, j;
+  for (i = 0; i < 7; i++) {
+    for (j = 0; j < 5; j++) {
+      move(y + j, x + i);
+      addch(' ');
+    }
+  }
+
+  move(3 + y, x);
+  printw(" %X  %X%X", card->stats[0], card->stats[1], card->stats[2]);
+  move(3 + y, 2 + x);
+  switch (card->type) {
+    case P:
+      addch('P');
+      break;
+    case M:
+      addch('M');
+      break;
+    case X:
+      addch('X');
+      break;
+    case A:
+      addch('A');
+      break;
+  }
+  for (i = 0; i < 8; i++) {
+    if (card->arrows & (1 << i)) {
+      draw_arrows(i, x, y, card->eq + 1);
+    }
+  }
+  move(0, 0);
+  attroff(COLOR_PAIR(card->eq + 1));
+  refresh();
+  return 0;
+}
+
+int draw_arrows(int i, int x, int y, int color) {
+  char arrows[] = "\\|/--/|\\";
+  int j = (((6 * (7 - i) / 5) / 3) * 2);
+  int k = (((5 * (7 - i) / 4) % 3) * 3);
+  attron(COLOR_PAIR(color));
+  move(j + y, k + x);
+  addch(arrows[i]);
+  move(0, 0);
+  attroff(COLOR_PAIR(color));
+  refresh();
+  return 0;
+}
+
+int redraw(struct state_t *state) {
+  if (state->phase == SELECTION_P) {
+    clear();
+    draw_table(winx, winy);
+    draw_cursor_b(winx + 1 + 10 * state->x, winy + 1 + 6 * state->y, 4);
+    draw_cursor_b(winx + 44 - (44 + 12) * (state->selection / 5) - 1,
+                  winy + 6 * (state->selection % 5) - 1, 6);
+    int i, j;
+    for (i = 0; i < 4; i++) {
+      for (j = 0; j < 4; j++) {
+        if (state->table[i][j] > 1) {
+          draw_card(&state->cards[0][state->table[i][j] - 2], winx + 2 + 10 * i,
+                    winy + 2 + 6 * j);
+        }
+        if (state->table[i][j] > 6) {
+          draw_card(&state->cards[1][state->table[i][j] - 7], winx + 2 + 10 * i,
+                    winy + 2 + 6 * j);
+        }
+      }
+    }
+    for (i = 0; i < 5; i++) {
+      if (state->cards[0][i].played == 0)
+        draw_card(&state->cards[0][i], winx + 44, winy + 6 * i);
+      if (state->cards[1][i].played == 0)
+        draw_card(&state->cards[1][i], winx - 12, winy + 6 * i);
+    }
+  }
+  if (state->phase == BATTLE_P) {
+    draw_card(state->battle.att, winx + 2 + 10 * state->battle.att->x,
+              winy + 2 + 6 * state->battle.att->y);
+    draw_arrows(state->evaluation.a_arr, winx + 2 + 10 * state->battle.att->x,
+                winy + 2 + 6 * state->battle.att->y, 5);
+    move(20, 10);
+    printw("Batalla ");
+    switch (state->battle.att->type) {
+      case P:
+        printw("física: ");
+        break;
+      case M:
+        printw("mágica: ");
+        break;
+      case X:
+        printw("X: ");
+        break;
+      case A:
+        printw("Alfa: ");
+        break;
+      default:
+        break;
+    }
+    printw("%X ", state->battle.param_att);
+    if (state->battle.resul == WIN) {
+      printw("es mayor que ");
+    } else if (state->battle.resul == LOSE) {
+      printw("es menor que ");
+    }
+    printw("%X", state->battle.param_def);
+    getch();
+  }
+  if (state->phase == COMBI_P) {
+    if (state->battle.combi > 0) {
+      move(21, 10);
+      printw("COMBI %d !!!", state->battle.combi);
+      getch();
+    }
+  }
+  refresh();
+  return 0;
+}
+
+int end_drawing() {
+  endwin();
+  return 0;
 }
--- main.h
+++ main.h
@@ -9,48 +9,48 @@
 #ifndef tetramaster_main_h
 #define tetramaster_main_h
 
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <curses.h>
 
-enum type_t {P,M,X,A};
-enum phase_t {SELECTION_P, GAME_P, BATTLE_P, COMBI_P};
+enum type_t { P, M, X, A };
+enum phase_t { SELECTION_P, GAME_P, BATTLE_P, COMBI_P };
 struct card_t {
-    int stats[3];
-    enum type_t type;
-    int arrows;
-    int eq;
-	int x,y;
-    int played;
+  int stats[3];
+  enum type_t type;
+  int arrows;
+  int eq;
+  int x, y;
+  int played;
 };
 
-enum evaluation_e {BATTLE, AUTOWIN, NONE};
-enum battle_e {WIN,LOSE};
+enum evaluation_e { BATTLE, AUTOWIN, NONE };
+enum battle_e { WIN, LOSE };
 
 struct battle_t {
-    enum type_t type;
-    struct card_t *att,*def;
-    int param_att;
-    int param_def;
-	int combi;
-    enum battle_e resul;
+  enum type_t type;
+  struct card_t *att, *def;
+  int param_att;
+  int param_def;
+  int combi;
+  enum battle_e resul;
 };
 struct evaluation_t {
-    enum evaluation_e resul;
-	int a_arr,d_arr;
+  enum evaluation_e resul;
+  int a_arr, d_arr;
 };
 
 struct state_t {
-    int x,y;
-    struct card_t cards[2][5];
-    int table[4][4];
-    int selection;
-    int turn;
-    enum phase_t phase;
-	
-	struct battle_t battle;
-	struct evaluation_t evaluation;
+  int x, y;
+  struct card_t cards[2][5];
+  int table[4][4];
+  int selection;
+  int turn;
+  enum phase_t phase;
+
+  struct battle_t battle;
+  struct evaluation_t evaluation;
 };
 
 int initgame(struct state_t *state);
@@ -58,21 +58,22 @@ int manage(struct state_t *state);
 int selection(struct state_t *state);
 void autowin(struct state_t *state, struct card_t *att, struct card_t *def);
 int insertcard(struct state_t *state, int eq, int num);
-int combi(struct state_t *state,struct card_t *card);
+int combi(struct state_t *state, struct card_t *card);
 int itojk(int i, int *j, int *k);
 int itojkxy(int i, int *j, int *k, int x, int y);
-int tbtocard (struct state_t *state, int *eq, int *num, int x, int y);
-enum evaluation_e evaluation(struct state_t *state,int x,int y,int i);
-enum battle_e battle(struct state_t *state, struct card_t *att, struct card_t *def);
-int game(struct state_t *state,int x, int y);
+int tbtocard(struct state_t *state, int *eq, int *num, int x, int y);
+enum evaluation_e evaluation(struct state_t *state, int x, int y, int i);
+enum battle_e battle(struct state_t *state, struct card_t *att,
+                     struct card_t *def);
+int game(struct state_t *state, int x, int y);
 int min(int a, int b);
 
 int init_drawing();
-int draw_table(int x,int y);
-int draw_card(struct card_t *card,int x,int y);
+int draw_table(int x, int y);
+int draw_card(struct card_t *card, int x, int y);
 int draw_cursor_a(int x, int y);
-int draw_cursor_b(int x, int y,int color);
-int draw_arrows(int i,int x, int y, int color);
+int draw_cursor_b(int x, int y, int color);
+int draw_arrows(int i, int x, int y, int color);
 int redraw(struct state_t *state);
 int end_drawing();
 int winx, winy;
